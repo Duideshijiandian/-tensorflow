7e387838fd2029faff1d26162edef2225ecd17ed
Duideshijiandian <3349691158@qq.com>
2026-05-31 16:48:42 +0800

    5月31日学习 - MNIST数据加载与预处理

---
+++ demo1.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+print(train_images.shape)
+print(test_images.shape)
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+model = tf.keras.Sequential(
+    [tf.keras.layers.Flatten(input_shape=(28, 28)),
+     tf.keras.layers.Dense(128),
+     tf.keras.layers.ReLU(),
+     tf.keras.layers.Dense(10)
+     ])
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+a =model.fit(train_images, train_labels, epochs=10)
+jieguo = tf.argmax(model.predict(test_images), axis=1)
+count = 0
+for i in range(10000):
+    if jieguo[i] == test_labels[i]:
+        count += 1
+print("准确率为：", count/10000)
+print(a.history['loss'][-1])
+
