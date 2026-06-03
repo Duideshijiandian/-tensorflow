14dad94945dc46a6c58b0175e78623e15a0887ed
你的GitHub用户名 <GitHub>
2026-06-04 03:23:18 +0800

    auto: session checkpoint 2026-06-04_03:23

--- scripts/cnn_application.py
+++ scripts/cnn_application.py
@@ -1,3 +1,24 @@
 import tensorflow as tf
 base = tf.keras.applications.MobileNetV2(weights='imagenet', include_top=False, input_shape=(96, 96, 3))
-print(base.summary())
+base.trainable = False
+for layer in base.layers[:-14]:
+    layer.trainable = True
+model = tf.keras.Sequential([base,
+                             tf.keras.layers.GlobalAveragePooling2D(),
+                             tf.keras.layers.Dense(256, activation='relu'),
+                             tf.keras.layers.Dropout(0.5),
+                             tf.keras.layers.Dense(5, activation='softmax')])
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+train_images = tf.image.resize(train_images, (96, 96))
+test_images = tf.image.resize(test_images, (96, 96))
+train_images = tf.keras.applications.mobilenet_v2.preprocess_input(train_images)
+test_images = tf.keras.applications.mobilenet_v2.preprocess_input(test_images)
+a = model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels), callbacks=[tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)])
+b = model.predict(test_images)
+b = tf.argmax(b, axis=1)
+count = 0
+for i in range(10000):
+    if b[i] == test_labels[i][0]:
+        count += 1
+print("准确率为：", count/10000)
